fe3fdccc808568a7f418546ffd671bf1d4f0df26
Ahlan Guarnier <ahlan90@gmail.com>
2020-02-17 10:41:10 -0300

    Signup feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
 
+    constructor(
+        private authService: AuthService
+        ) {}
+    
     @Post('/signup')
-    singUp(@Body() authCredentialsDto: AuthCredentialsDto) {
-        console.log('Auth', authCredentialsDto);
-        
+    async singUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return await this.authService.signUp(authCredentialsDto);
     }
 }
